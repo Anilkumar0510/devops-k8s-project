b037fce43171e98dbce3c8335604a2c27279dfb8
mmbabu0705 <mmbabu0705@gmail.com>
2025-02-08 11:39:23 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "WELCOME TO DEVOPS SHACK";
+		return "WELCOME TO TEKS-ACADEMY";
 	}
 
 	public static void main(String[] args) {
